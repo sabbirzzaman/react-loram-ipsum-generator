6ecb879709fade0196b41944b43ed79c8e22d814
Tonmoy <sztonmoy7@gmail.com>
2021-04-12 18:48:15 +0600

    React Lorem Ipsum Generator Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import data from "./data";
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+  const [alert, setAlert] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+
+    if (count > 50) {
+      amount = 50;
+    }
+    setText(data.slice(0, amount));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section-center">
+      <h3>Lorem Ipsum Generator</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button className="btn" onClick={() => { setAlert(true); }} >
+          GENARATE!
+        </button>
+      </form>
+      {alert && (
+        <article className="lorem-text">
+          {text.map((item, index) => {
+            return <p key={index}>{item}</p>;
+          })}
+        </article>
+      )}
+    </section>
   );
 }
 
